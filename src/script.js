0c59515827cfb22851be263e2cfc82329f1121aa
Nicole Zheng <zznicole@hotmail.com>
2020-05-11 23:06:34 +0200

    init commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat:58.413740,lng:15.623510},
+  zoom: 14,
+  icon: 
+  'https://www.tiles4all.co.za/wp-content/sabai/File/thumbnails/4e68d5d175ef448e8eb3b14cb72083a9.png'
+});
